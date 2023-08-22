2562beb51c8ce8ddff4792689e45eba195a36d00
Leonard Mbasela <132356293+staleon-codes@users.noreply.github.com>
2023-08-22 12:34:48 +0200

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -1,43 +1,37 @@
 #include "shell.h"
-
 /**
- * _memset - fills memory with a constant byte
- * @s: the pointer to the memory area
- * @b: the byte to fill *s with
- * @n: the amount of bytes to be filled
- * Return: (s) a pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
-}
-
-/**
- * ffree - frees a string of strings
- * @pp: string of strings
+ * ffree - Releases memory allocated for a string of strings
+ * @pp: String of strings to be freed
  */
 void ffree(char **pp)
 {
 	char **a = pp;
-
 	if (!pp)
-		return;
+	return;
 	while (*pp)
-		free(*pp++);
+	free(*pp++);
 	free(a);
 }
-
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
- *
- * Return: pointer to da ol'block nameen.
+ * _memset - Fills a memory area with a constant byte
+ * @s: Pointer to the memory area
+ * @b: The byte value to fill 's' with
+ * @n: The number of bytes to be filled
+ * Return: A pointer to the memory area 's'
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	s[i] = b;
+	return (s);
+}
+/**
+ * _realloc - Reallocates a block of memory
+ * @ptr: Pointer to the previous dynamically allocated block
+ * @old_size: Byte size of the previous block
+ * @new_size: Byte size of the new block
+ * Return: Pointer to the reallocated block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
