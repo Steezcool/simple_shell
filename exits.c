db2a1c2c56c9dc18caf6a3d6624d28e173832498
Leonard Mbasela <132356293+staleon-codes@users.noreply.github.com>
2023-08-22 11:03:40 +0200

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,74 +1,72 @@
 #include "shell.h"
-
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * _strchr - Finds the first incidence of a character in a string
+ * @s: The string to be searched
+ * @c: The character to be located
+ * Return: A pointer to the memory area 's'
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strchr(char *s, char c)
 {
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
+	do {
+	if (*s == c)
 	return (s);
+	} while (*s++ != '\0');
+	return (NULL);
 }
-
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * _strncat - integrates two strings
+ * @dest: The first string
+ * @src: The second string
+ * @n: The maximum number of bytes to be used
+ * Return: The integrated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
-
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
-		i++;
+	i++;
 	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
 	if (j < n)
-		dest[i] = '\0';
+	dest[i] = '\0';
+	return (s);
+}
+/**
+ * _strncpy - This function Copies a string
+ * @dest: The destination string to copy to
+ * @src: The source string
+ * @n: The number of characters to copy
+ * Return: The resulting copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+	dest[i] = src[i];
+	i++;
+	}
+	if (i < n)
+	{
+	j = i;
+	while (j < n)
+	{
+	dest[j] = '\0';
+	j++;
+	}
+	}
 	return (s);
 }
 
-/**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
- */
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
 
-	return (NULL);
-}
+
+
