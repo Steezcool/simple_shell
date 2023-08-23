463aab48bd54ca4f6daf211be389de9b36b50d2a
Steezcool <petermcmillan25@gmail.com>
2023-08-23 04:58:45 -0700

    vars.c

--- memory.c
+++ memory.c
@@ -1,16 +1,18 @@
 #include "shell.h"
+
 /**
- * bfree - This function Frees a pointer and sets the address to NULL
- * @ptr: Address of the pointer to free
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
  * Return: 1 if freed, otherwise 0.
  */
 int bfree(void **ptr)
 {
 	if (ptr && *ptr)
 	{
-	free(*ptr);
-	*ptr = NULL;
-	return (1);
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
 	}
 	return (0);
 }
