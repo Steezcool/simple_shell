946a510aae239e5e1d97f7c41f09c2835eca7a9a
Steezcool <petermcmillan25@gmail.com>
2023-08-23 05:19:24 -0700

    tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -11,6 +11,7 @@ char **strtow2(char *str, char d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
+
 	if (str == NULL || str[0] == 0)
 	return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
@@ -54,6 +55,7 @@ char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
+
 	if (str == NULL || str[0] == 0)
 	return (NULL);
 	if (!d)
