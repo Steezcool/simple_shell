f8f71a5cd9ff20cc2baecda5d684849547ae2c24
Steezcool <petermcmillan25@gmail.com>
2023-08-23 06:09:04 -0700

    exits.c

--- exits.c
+++ exits.c
@@ -24,6 +24,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
+
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
@@ -48,7 +49,9 @@ char *_strncat(char *dest, char *src, int n)
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
+
 	char *s = dest;
+
 	i = 0;
 	while (src[i] != '\0' && i < n - 1)
 	{
