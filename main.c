e70476fb75ba037d5807e33149627a6440c90fcf
Leonard Mbasela <132356293+staleon-codes@users.noreply.github.com>
2023-08-23 16:27:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -22,18 +22,18 @@ int main(int ac, char **av)
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
+		if (errno == EACCES)
+		exit(126);
+		if (errno == ENOENT)
+		{
+		_eputs(av[0]);
+		_eputs(": 0: Can't open ");
+		_eputs(av[1]);
+		_eputchar('\n');
+		_eputchar(BUF_FLUSH);
+		exit(127);
+		}
+		return (EXIT_FAILURE);
 		}
 		info->readfd = fd;
 	}
