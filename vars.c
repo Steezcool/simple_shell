0fb23311acdf54722cb17da5e74646c698b7de25
Steezcool <petermcmillan25@gmail.com>
2023-08-23 04:52:37 -0700

    vars.c

--- vars.c
+++ vars.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * check_chain - Checks whether chaining should continue based on the last status
+ * check_chain - checks whether chaining should continue based on last status
  * @info: The parameter struct
  * @buf: The character buffer
  * @p: Address of the current position in 'buf'
@@ -11,6 +11,7 @@
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
+
 	if (info->cmd_buf_type == CMD_AND)
 	{
 	if (info->status)
@@ -40,6 +41,7 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 int is_chain(info_t *info, char *buf, size_t *p)
 {
 	size_t j = *p;
+
 	if (buf[j] == '|' && buf[j + 1] == '|')
 	{
 	buf[j] = 0;
@@ -84,6 +86,7 @@ int replace_alias(info_t *info)
 	int i;
 	list_t *node;
 	char *p;
+
 	for (i = 0; i < 10; i++)
 	{
 	node = node_starts_with(info->alias, info->argv[0], '=');
@@ -109,7 +112,9 @@ int replace_alias(info_t *info)
 int replace_vars(info_t *info)
 {
 	int i = 0;
+
 	list_t *node;
+
 	for (i = 0; info->argv[i]; i++)
 	{
 	if (info->argv[i][0] != '$' || !info->argv[i][1])
